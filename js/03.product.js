8082e4403fe8d5588a520b159ab32f2ecb6ed68a
JEONMINJU <junielle.jeon@gmail.com>
2021-03-03 13:32:52 +0900

    first-commits

--- js/03.product.js
+++ js/03.product.js
@@ -1,18 +1,35 @@
 /**************** App ******************/
 class App extends React.Component {
+	products = []
 	state = {
-		query: ''
+		query: '',
+		resultProducts: []
 	}
-	handleChange = value => {
+	commitChange = value => {
 		this.setState({
-			query: value
+			query: value,
+			resultProducts: this.products.filter(v => v.title.includes(value))
+		})
+	}
+	componentDidMount() {
+		axios.get('../json/products.json')
+		.then(r => {
+			this.setState({
+				resultProducts: r.data,
+			})
+			this.products = r.data
+		})
+		.catch(e => {
+			console.log(e)
 		})
 	}
 	render() {
+		let { query, resultProducts } = this.state 
 		return (
 			<div className="container">
-				<TitleBar query={this.state.query} className="my-4" />
-				<Search onChange={this.handleChange} />
+				<TitleBar query={query} className="my-4" />
+				<Search onChange={this.commitChange} />
+				<Products products={resultProducts} />
 			</div>
 		)
 	}
@@ -36,16 +53,68 @@ class TitleBar extends React.Component {
 
 /**************** Search ******************/
 class Search extends React.Component {
+	state = {
+		query: ''
+	}
+	inputRef = React.createRef() // DOM을 품는 변수가 되어라
 	handleChange = e => {
-		this.props.onChange(e.target.value)
+		this.onChange(e.target.value)
+	}
+	handleRemove = e => {
+		this.onChange('')
+		// ref는 current로 접근하여야 한다.
+		this.inputRef.current.focus()
+	}
+	onChange = value => {
+		this.props.onChange(value)
+		this.setState({
+			query: value
+		})
 	}
 	render() {
 		return (
 			<form className="form-search">
-				<input className="form-control" type="text" onChange={this.handleChange} autoFocus />
+				<input className="form-control py-2" type="text" onChange={this.handleChange} autoFocus value={this.state.query} ref={this.inputRef /* 나를 inputRef에 담아라 */} />
+				{
+					this.state.query.length > 0
+					? <i className="fa fa-times-circle bt-remove" onClick={this.handleRemove} />
+					: ''
+				}
 			</form>
 		)
 	}
 }
+
+/**************** Products ******************/
+class Products extends React.Component {
+	render() {
+		return (
+			<div className="list-wrapper">
+				{ this.props.products.map(v => <Product value={v} key={v.id+'_'+v.title}/>) }
+			</div>
+		)
+	}
+}
+
+/**************** Product ******************/
+class Product extends React.Component {
+	render() {
+		let { src, title, price } = this.props.value
+		return (
+			<div className="list">
+				<div className="img-wrap">
+					<img src={src} className="w-100"/>
+				</div>
+				<div className="title">{title}</div>
+				<div className="price">${price}</div>
+			</div>
+		)
+	}
+}
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'))
